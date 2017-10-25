1f38cb2d8d74d86ea3daea644194934bc5312654
guozhi <990135368@qq.com>
2017-10-25 21:32:56 +0800

    添加项目文件。

---
+++ OAS/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OAS.Startup))]
+namespace OAS
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
